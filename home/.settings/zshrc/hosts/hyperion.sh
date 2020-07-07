e9a0b2edf047b7d25ceabef605e53ed17be11446
Vincent Leroy <vincent.leroy@rayference.eu>
2020-07-07 10:37:27 +0200

    Fixed iterm2 error messages on hyperion

--- home/.settings/zshrc/hosts/hyperion.sh
+++ home/.settings/zshrc/hosts/hyperion.sh
@@ -16,3 +16,9 @@ config[homebrew]="yes"
 # Fix broken locales
 # export LC_ALL=en_US.UTF-8
 # export LANG=en_US.UTF-8
+
+# Fix iterm2 error messages
+if ! type iterm2_print_user_vars | grep -q '^function$' 2>/dev/null; then
+    # echo "Defining iterm2_print_user_vars"
+    iterm2_print_user_vars() {}
+fi

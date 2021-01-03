b4f8713b7d1626e633aacd31544151a517579a54
Vincent Leroy <vincent.leroy@rayference.eu>
2021-01-03 12:23:10 +0100

    Add script for host tremere

---
+++ home/.settings/zshrc/hosts/tremere.sh
@@ -0,0 +1,10 @@
+# Basic directories
+config[settings_directory]=$HOME/.settings
+config[applications_directory]=$HOME/Applications
+config[source_directory]=$HOME/Documents/src
+
+# Miniconda
+config[miniconda]=$HOME/miniconda3
+
+# Go
+config[go]=$HOME/go

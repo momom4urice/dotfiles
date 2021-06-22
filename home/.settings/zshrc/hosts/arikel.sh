464e373a2c176334963ee65caed3aaf800819741
Vincent Leroy <vincent.leroy@rayference.eu>
2021-06-22 08:55:23 +0200

    arikel: Switch to mambaforge

--- home/.settings/zshrc/hosts/arikel.sh
+++ home/.settings/zshrc/hosts/arikel.sh
@@ -4,7 +4,13 @@ config[applications_directory]=$HOME/Applications
 config[source_directory]=$HOME/Documents/src
 
 # Miniconda
-config[miniconda]=$HOME/miniconda3
+config[miniconda]=$HOME/mambaforge
 
 # Go
 config[go]=$HOME/go
+
+# Fix iterm2 error messages
+if ! type iterm2_print_user_vars | grep -q '^function$' 2>/dev/null; then
+    # echo "Defining iterm2_print_user_vars"
+    iterm2_print_user_vars() {}
+fi
